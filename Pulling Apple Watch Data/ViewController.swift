b5bee93ca71ee2a27c30872edb2999b3b5fb1628
John W. Smith, V <jwsv61099@gmail.com>
2021-02-06 14:25:54 -0500

    Initial Commit

---
+++ Pulling Apple Watch Data/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pulling Apple Watch Data
+//
+//  Created by John Smith V on 2/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pulling Apple Watch DataTests/Pulling_Apple_Watch_DataTests.swift
@@ -0,0 +1,33 @@
+//
+//  Pulling_Apple_Watch_DataTests.swift
+//  Pulling Apple Watch DataTests
+//
+//  Created by John Smith V on 2/6/21.
+//
+
+import XCTest
+@testable import Pulling_Apple_Watch_Data
+
+class Pulling_Apple_Watch_DataTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
